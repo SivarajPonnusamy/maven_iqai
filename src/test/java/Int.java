087bcbe71d3303e1a760141d86b02b7c4db4aa89
Sivaraj Ponnusamy <rajsivasj@gmail.com>
2024-07-26 12:43:35 +0530

    1st commit after resignation

---
+++ src/test/java/Int.java
@@ -0,0 +1,18 @@
+
+public class Int {
+	
+	    public static void main(String[] args) {
+	        String str="kgk";
+	        int i=10;
+	      try {
+	        i = Integer.parseInt(str);
+	      }
+	      catch(Exception e) {
+	 
+	            
+				System.out.println(i);
+	      }
+	    }
+	}
+
+
