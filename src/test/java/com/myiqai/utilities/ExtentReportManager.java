da443907f741af141bf09634e1032b3dcfd582ed
SivarajPonnusamy <rajsivasj@gmail.com>
2024-08-13 15:26:08 +0530

    extent reports

---
+++ src/test/java/com/myiqai/utilities/ExtentReportManager.java
@@ -0,0 +1,73 @@
+package com.myiqai.utilities;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+public class ExtentReportManager implements ITestListener {
+	public ExtentSparkReporter sparkReporter;// UI of the report
+	public ExtentReports extent; // populate common info on the report
+	public ExtentTest test;// creating testcase entries in the report and update status of the test methods
+	
+	public void onStart(ITestContext context) {
+		sparkReporter = new ExtentSparkReporter(System.getProperty("user.dir"+ "/reports/myreports.html"));
+		sparkReporter.config().setDocumentTitle("MYIQ.AI Automation Report");
+		sparkReporter.config().setReportName("Regression testing");
+		sparkReporter.config().setTheme(Theme.STANDARD);
+		
+		extent= new ExtentReports();
+		extent.attachReporter(sparkReporter);
+		
+		extent.setSystemInfo("Computer name", "local host Mac");
+		extent.setSystemInfo("Environment", "QA");
+		extent.setSystemInfo("Tester Name", "Siva");
+		extent.setSystemInfo("OS", "Mac OS");
+		extent.setSystemInfo("Browser name", "Chrome");
+
+	}
+
+	
+	public void onTestStart(ITestResult result) {
+		
+		
+	}
+
+	
+	public void onTestSuccess(ITestResult result) {
+		test=extent.createTest(result.getName());
+		
+	}
+
+	
+	public void onTestFailure(ITestResult result) {
+		
+		
+	}
+
+	
+	public void onTestSkipped(ITestResult result) {
+		
+		
+	}
+
+	
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		
+		
+	}
+
+	
+	public void onFinish(ITestContext context) {
+		
+		
+	}
+	
+	
+
+}
